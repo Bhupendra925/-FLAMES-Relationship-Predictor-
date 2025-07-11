975760e748d5106519baee8e1a274d526f4ed9c0
Bhupendra Kumar Vishwakarma <bhupendravi925@gmail.com>
2025-07-11 18:15:30 +0530

    Add files via upload

---
+++ flame__game.py
@@ -0,0 +1,23 @@
+name1 = list(input("Enter your name : ").lower())
+name2 = list(input("Enter your  partner\s name: ").lower())
+
+for latter in name1[:]: #[] ye slace karega remove karne ke bad suru se count / read karega word ko
+    if latter in name2:
+        name1.remove(latter)
+        name2.remove(latter)
+    
+count = len(name1)+ len(name2)
+
+flames = ["Friends" , " Love" , "Affection" , " Marrige" , "Enemy" ,"Sibling"]
+
+i =0
+current_count =0
+
+while len(flames)> 1:
+    if current_count == count - 1:
+        flames.pop(i)
+        current_count =0
+    i = (i+1)% len(flames)
+    current_count += 1
+
+print( "Your Reletionshipis :- ", flames[0])
